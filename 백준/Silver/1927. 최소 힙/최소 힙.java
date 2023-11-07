ab09fc1a9f539f2eb93627e0c6e8c22f317042df
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2023-11-07 21:48:05 +0900

    [Silver II] Title: 최소 힙, Time: 1492 ms, Memory: 33700 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x > 0) {
+				pq.add(x);
+			} else {
+				if(pq.size() == 0) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			}
+		}
+	}
+}
