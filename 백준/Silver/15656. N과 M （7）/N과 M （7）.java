9282cec2a8036e0f8700962466fd6e3bc00d23c9
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2023-12-08 16:28:40 +0900

    [Silver III] Title: N과 M (7), Time: 700 ms, Memory: 224860 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -2,40 +2,37 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
-	
-	static int N;
-	static int M;
-	static int[] nums;
-	static int[] sel;
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	static int N  = sc.nextInt();; //N개의 숫자 중
+	static int M = sc.nextInt(); //M개 고르기
+	static int[] arr = new int[N];
+	static int[] sel = new int[M];
+//	static boolean[] visited = new boolean[N];
 	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		nums = new int[N];
-		sel = new int[M];
-		
+		arr = new int[N];
 		for (int i = 0; i < N; i++) {
-			nums[i] = sc.nextInt();
+			arr[i] = sc.nextInt();
 		}
-		Arrays.sort(nums);
-		perm(0, nums);
+		Arrays.sort(arr); //사전순으로 출력하기 위해 정렬
+		perm(0, arr);
 		System.out.println(sb);
 	}
-	
-	static void perm(int idx, int[] nums) {
-		if(idx == M) {
+
+	private static void perm(int idx, int[] arr) {
+		if(idx==M) { //M개 다 뽑았으면 return
 			for (int i : sel) {
 				sb.append(i).append(" ");
 			}
 			sb.append("\n");
 			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			sel[idx] = nums[i];
-			perm(idx+1, nums);
+		} else {
+			for (int i = 0; i < N; i++) {
+				sel[idx] = arr[i];
+				perm(idx+1, arr);
+			}
 		}
 	}
 }
