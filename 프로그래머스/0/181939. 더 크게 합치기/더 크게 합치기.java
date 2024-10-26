0bec6ff3560a6d6ddc46548bb4e123f9230921c5
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2024-10-26 23:15:27 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.78 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+        
+        if (ab > ba) {
+            answer = ab;
+        } else if (ba > ab) {
+            answer = ba;
+        } else {
+            answer = ab;
+        }
+
+        return answer;
+    }
+}
