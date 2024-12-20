46561dd701588d5ea01b4245b27cb9fc3eebfc34
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2024-12-20 23:52:37 +0900

    Update 핸드폰 번호 가리기.java

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,10 +1,14 @@
 class Solution {
     public String solution(String phone_number) {
         String answer;
+
+        //string -> char[] 메서드 !!!!
         char[] array = phone_number.toCharArray();
         for(int i = 0; i < phone_number.length()-4; i++) {
             array[i] = '*';
         }
+
+        //char[] -> string 생성자 !!!!
         answer = new String(array);
         return answer;
     }
