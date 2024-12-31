4cf3110b5ab60dcf8c97f6de8115ddbd58e9cfbd
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2024-12-31 19:45:10 +0900

    [Silver IV] Title: 수 찾기, Time: 1804 ms, Memory: 186736 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,53 +1,44 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+
+		//N개의 수를 가진 배열에서 M개의 수가 있으면 1, 없으면 0 출력
 		
-		int N = Integer.parseInt(st.nextToken());
-		int[] A = new int[N];
+		Scanner sc = new Scanner(System.in);
 		
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
 		}
-		Arrays.sort(A);
+		int M = sc.nextInt();
+		Arrays.sort(arr);
 		
-		st = new StringTokenizer(br.readLine(), " ");
-		int M = Integer.parseInt(st.nextToken());
-		int[] B = new int[M];
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < M; i++) {
-			B[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < M; i++) {
+			int key = sc.nextInt();
+			System.out.println(binarySearch(arr, key));
 		}
-		for (int i = 0; i < M; i++) {
-			int sta = 0;
-			int end = N-1;
-			int mid;
-			boolean check = false;
-			
-			 while(sta <= end) {
-				mid = (sta+end)/2;
-				if(A[mid] == B[i]) {
-					sb.append(1).append('\n');
-					check = true;
-					break;
-				} else if(A[mid] < B[i]) {
-					sta = mid+1;
-				} else {
-					end = mid-1;
-				}
-			}
-			if(!check) sb.append(0).append("\n");
-		}
-		System.out.println(sb);
+		
 	}
+
+	private static int binarySearch(int[] arr, int key) {
+		int start = 0;
+		//length에서 1 빼주지 않으면 인덱스 꼬임...!!!
+		int end = arr.length - 1;
+		
+		// < 아니고 <= 이 되어야 함
+		// start == end 인 경우는 검사 안 하고 넘어가게 됨
+		
+		while(start <= end) {
+			int middle = (start + end) / 2;
+			if(arr[middle] == key) return 1;
+			if(arr[middle] < key) start = middle + 1;
+			if(arr[middle] > key) end = middle - 1;
+		}
+		return 0;
+	}
+
 }
