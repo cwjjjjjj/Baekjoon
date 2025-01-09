bd33ad22a28ba5d24dd69ed5d09e04c065031250
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2025-01-09 23:28:57 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,7 @@
 import java.io.*;
-import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
         StringBuilder sb = new StringBuilder(my_string);
         return sb.reverse().toString();
     }
