5b8588f17c4ffaf7cebb32a4b2327df4b3eaacd9
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2025-01-13 23:57:24 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.06 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str_list.length; i++) {
+            if(!str_list[i].contains(ex)) {
+                sb.append(str_list[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
