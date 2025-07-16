0893cc4d94cb5b6052cc5bd39fabc0a2f7da044f
woojin cheon <123845033+cwjjjjjj@users.noreply.github.com>
2025-07-16 21:39:39 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n / slice;
+        if(n % slice > 0) {
+            answer++;
+        }
+        if(answer == 0) {
+            return 1;
+        }
+        return answer;
+    }
+}
